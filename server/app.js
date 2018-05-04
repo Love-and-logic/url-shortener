1af1245ae4f7016eac0e38c7b1de1ea221594f46
Morgan <31396698+rainbyrd@users.noreply.github.com>
2018-05-04 15:34:33 -0700

    Friday lab group work

--- server/app.js
+++ server/app.js
@@ -5,13 +5,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const routes = require('./routes')
 
-let store = {
-    posts: [{
-        url: 'https://courses.edx.org/courses/course-v1:Microsoft+DEV283x+2T2017/course/',
-        text: 'ss.',
-        }]
-    }]
-}
 
 let app = express()
 
@@ -21,24 +14,8 @@ app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(errorhandler())
 app.use(cors())
-app.use((req, res, next) => {
-    req.store = store
-    next()
-})
-app.get('/', routes.sayHey)
-app.post('/response', routes.postForm)
-/*
-app.get('/posts', routes.posts.getPosts)
-app.get('/posts/:postId', routes.posts.getPost)
-app.post('/posts', routes.posts.addPost)
-app.put('/posts/:postId', routes.posts.updatePost)
-app.delete('/posts/:postId', routes.posts.removePost)
 
-//
-app.get('/posts/:postId/comments', routes.comments.getComments)
-app.post('/posts/:postId/comments', routes.comments.addComment)
-app.put('/posts/:postId/comments/:commentId', routes.comments.updateComment)
-app.delete('/posts/:postId/comments/:commentId', routes.comments.removeComment)
-*/
+app.post('/response', routes.postForm)
+
 
 app.listen(3000)
